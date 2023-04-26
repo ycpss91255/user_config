3d9e1ea998f9fd4e78493f5bba62e5a51dd644cf
cyc <ycpss91255@gmail.com>
2023-04-26 11:29:08 +0800

    add: confirm ubuntu-drivers devices not working in docker

--- nvidia/install.sh
+++ nvidia/install.sh
@@ -3,14 +3,26 @@
 installed_nvidia_drivers=$(dpkg --get-selections | grep -oP '^nvidia-driver-\d+' | head -1)
 
 if [ -z "${installed_nvidia_drivers}" ]; then
-    sudo add-apt-repository -y ppa:graphics-drivers/ppa \
+    sudo apt update \
     && sudo apt install -y --no-install-recommends \
+        software-properties-common \
         ubuntu-drivers-common \
-    && recommended_drivers=$(ubuntu-drivers devices | grep 'recommended' | awk '{print $3}') \
-    && sudo apt update \
-    && sudo apt install -y --no-install-recommends \
-        "${recommended_drivers}" \
-    && nvidia-smi
+        gnupg-agent \
+    && sudo add-apt-repository -y ppa:graphics-drivers/ppa
+
+    # BUG: docker not work, wait test to normal system
+    recommended_drivers=$(ubuntu-drivers devices | grep 'recommended' | awk '{print $3}')
+
+    if [ -n "${recommended_drivers}" ]; then
+        sudo apt update \
+        && sudo apt install -y --no-install-recommends \
+            "${recommended_drivers}" \
+        && echo "Recommended drivers: ${recommended_drivers}" \
+        && nvidia-smi
+    else
+        echo "No recommended drivers found"
+        exit 1
+    fi
 else
     echo "Nvidia drivers already installed: ${installed_nvidia_drivers}"
 fi
