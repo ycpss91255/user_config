52e1399c65e5f768052e64cfc637db37d2cb6f1e
cyc <ycpss91255@gmail.com>
2023-04-26 16:07:23 +0800

    del: extra stuff

--- tmux/tmux_status.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cpu=$(top -bn1 | awk '/Cpu\(s\)/ {printf "%.0f%%", 100 - $8}')
-ram=$(free | awk 'NR==2{printf "%.0f%%", $3/($3+$4+$6+$7)*100}')
-
-echo "\
-#[fg=white, bg=green]C: $cpu#[fg=default, bg=default]\
-#[bg=default] #[bg=default]\
-#[fg=white, bg=cyan]M: $ram#[fg=default, bg=default]"
