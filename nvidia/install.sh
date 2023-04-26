89d8c5685f6096003d04bbb1857648f2782e6833
cyc <ycpss91255@gmail.com>
2023-04-26 10:35:03 +0800

    add: confirm whether nvidia driver has been installed

--- nvidia/install.sh
+++ nvidia/install.sh
@@ -2,10 +2,15 @@
 
 installed_nvidia_drivers=$(dpkg --get-selections | grep -oP '^nvidia-driver-\d+' | head -1)
 
-sudo add-apt-repository -y ppa:graphics-drivers/ppa \
-&& sudo apt install -y --no-install-recommends \
-    ubuntu-drivers-common \
-&& recommended_drivers=$(ubuntu-drivers devices | grep 'recommended' | awk '{print $3}') \
-&& sudo apt update \
-&& sudo apt install -y --no-install-recommends \
-    "${recommended_drivers}"
+if [ -z "${installed_nvidia_drivers}" ]; then
+    sudo add-apt-repository -y ppa:graphics-drivers/ppa \
+    && sudo apt install -y --no-install-recommends \
+        ubuntu-drivers-common \
+    && recommended_drivers=$(ubuntu-drivers devices | grep 'recommended' | awk '{print $3}') \
+    && sudo apt update \
+    && sudo apt install -y --no-install-recommends \
+        "${recommended_drivers}" \
+    && nvidia-smi
+else
+    echo "Nvidia drivers already installed: ${installed_nvidia_drivers}"
+fi
