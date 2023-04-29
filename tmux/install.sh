685ab2085ff5860a978bded51692e9382858a795
cyc <ycpss91255@gmail.com>
2023-04-30 07:23:34 +0800

    adjust: extra stuff and git clone --depth

--- tmux/install.sh
+++ tmux/install.sh
@@ -9,6 +9,7 @@ sudo apt update && \
 sudo apt install -y --no-install-recommends \
     git \
     tmux && \
-git clone https://github.com/tmux-plugins/tpm /home/"${username}"/.tmux/plugins/tpm && \
+rm -rf /home/"${username}"/.tmux/plugins/tpm >/dev/null 2>&1 && \
+git clone --depth 1 https://github.com/tmux-plugins/tpm /home/"${username}"/.tmux/plugins/tpm && \
 cp "${file_dir}"/tmux.conf /home/"${username}"/.tmux.conf && \
 /home/"${username}"/.tmux/plugins/tpm/scripts/install_plugins.sh
