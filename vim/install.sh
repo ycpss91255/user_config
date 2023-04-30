b43003f5eebb6f65fa575271861c93194af350b9
cyc <ycpss91255@gmail.com>
2023-04-30 16:42:24 +0800

    fix: curl not found

--- vim/install.sh
+++ vim/install.sh
@@ -7,6 +7,7 @@ username=${1:-"$USER"}
 sudo apt update && \
 sudo apt install -y --no-install-recommends \
     git \
+    curl \
     vim && \
 curl -fLo /home/"${username}"/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
