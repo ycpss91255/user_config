642c54702a582167ccd4d0304e105c0d3e886699
cyc <ycpss91255@gmail.com>
2023-04-30 16:51:18 +0800

    add: ssh git folder

---
+++ git/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# ${1}: USER NAME
+
+# file_dir=$(dirname "$(readlink -f "${0}")")
+# username=${1:-"$USER"}
+
+# sudo apt update && \
+# sudo apt install -y --no-install-recommends \
---
+++ ssh/remove.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# ${1}: USER NAME
+
+# file_dir=$(dirname "$(readlink -f "${0}")")
+# username=${1:-"$USER"}
+
+# sudo apt purge -y \
