c7a436376af05004c8344d9bb2f339e10c60b914
cyc <ycpss91255@gmail.com>
2023-05-01 16:59:24 +0800

    fix: copy vimrc path error

--- vim/install.sh
+++ vim/install.sh
@@ -3,6 +3,7 @@
 # ${1}: USER NAME
 
 username=${1:-"$USER"}
+file_dir=$(dirname "$(readlink -f "${0}")")
 
 sudo apt update && \
 sudo apt install -y --no-install-recommends \
@@ -11,5 +12,5 @@ sudo apt install -y --no-install-recommends \
     vim && \
 curl -fLo /home/"${username}"/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && \
-cp vimrc /home/"${username}"/.vimrc && \
+cp "${file_dir}"/vimrc /home/"${username}"/.vimrc && \
 vim -es -u /home/"${username}"/.vimrc -i NONE -c "PlugInstall" -c "qa"
