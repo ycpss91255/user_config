88d1271dc4a6a717777057bd644ff81356294ed6
cyc <ycpss91255@gmail.com>
2023-05-01 18:26:18 +0800

    add: remove .docker

--- docker/remove.sh
+++ docker/remove.sh
@@ -10,8 +10,8 @@ if (lspci | grep -q VGA ||
     nvidia-smi -L >/dev/null 2>&1 | grep -iq nvidia) &&
     (command -v nvidia-smi >/dev/null 2>&1); then
     sudo apt remove -y --purge \
-        nvidia-container-toolkit && \
-    sudo rm /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg && \
+        nvidia-container-toolkit
+    sudo rm /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
     sudo rm /etc/apt/sources.list.d/nvidia-container-toolkit.list
 fi
 
@@ -20,11 +20,12 @@ sudo apt purge -y \
     docker-ce-cli \
     containerd.io \
     docker-buildx-plugin \
-    docker-compose-plugin && \
-sudo rm /etc/apt/keyings/docker.gpg && \
-sudo rm /etc/apt/sources.list.d/docker.list && \
-sudo groupdel docker && \
-sudo gpasswd -d "${username}" docker && \
-sudo rm -rf /home/"${username}"/.docker && \
-sudo systemctl disable docker.service && \
+    docker-compose-plugin
+sudo rm -rf /etc/apt/keyings/docker.gpg \
+    /etc/apt/sources.list.d/docker.list \
+    /home/"${username}"/.docker
+sudo groupdel docker
+sudo gpasswd -d "${username}" docker
+sudo rm -rf /home/"${username}"/.docker
+sudo systemctl disable docker.service
 sudo systemctl disable containerd.service
